e8428d3035c5840f7982e3b2eb27ff125238af14
forkingits1123 <lblack11235813@gmail.com>
2021-05-26 10:58:24 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const superbowlWin = objectArray => {
+    const r = objectArray.find(element => element.result === 'W');
+    if (r) {
+        return r.year
+    } else {
+        return undefined;
+    }
+}
